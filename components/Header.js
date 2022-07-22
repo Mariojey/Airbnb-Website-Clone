d446fc0535a18392745d3da395827c81fbb18484
Mariusz Jacek <marios.yaceg@gmail.com>
2022-07-22 09:15:29 +0200

    Init

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <img src="../images/photo-grid.png" className="header-img"/>
+            <h1 className="header-text">Online Experiences</h1>
+            <p className="header-text-p">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </div>
+    )
+}
