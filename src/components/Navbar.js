86821dcffe28e848901487e1c795d14680de808e
Mariusz Jacek <marios.yaceg@gmail.com>
2022-08-02 12:08:46 +0200

    map data

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../style.css"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="../public/images/airbnb-logo.png" className="nav--logo" />
+        </nav>
+    )
+}
