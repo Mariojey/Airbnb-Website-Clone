b27f9631b7757602deda35cd00ef868551382bb6
Mariusz Jacek <marios.yaceg@gmail.com>
2022-08-05 09:23:02 +0200

    sold out if

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,16 @@ import React from "react"
 import '../style.css'
 
 export default function Card(props) {
+    console.log(props.openSpots)
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    }else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
+            {props.openSpots === 0 && <div className="card--badge">{badgeText}</div>}
             <img src={`../images/${props.img}`} className="card--image" />
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star" />
